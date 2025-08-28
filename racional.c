70bf974a18649898c1ce77fb7917a99241cb24b6
Bruno Yuuki Hayashi <byh25@atlas>
2025-08-28 09:50:11 -0300

    finalizei as funções de racional.h (operações aritméticoas), no entanto, aguardo a resposta do professor a respeito do seu retorno quando inválida. Falta modificar o arquivo tp1.c

--- racional.c
+++ racional.c
@@ -149,3 +149,42 @@ void imprime_r (struct racional r) {
   }
 }
 
+struct racional soma_r (struct racional r1, struct racional r2){
+  if ((!validor_r(r1)||(!valido_r(r2))))
+    return();//inválido
+  
+  struct racional rSoma;
+  rSoma.den= mmc(r1.den, r2.den); //denominador do r resultado da soma, necessita do mmc dos denominadores dos racionais a serem somados
+  rSoma.num= ((r1.num*(rSoma.den/r1.den))+(r2.num*(rSoma.den/r2.den))); //o numerador do r resultado, é a soma dos numeradores dos racionais, cada um multiplicado do mmc dos denominadores
+  return(rSoma);
+}
+
+struct racional subtrai_r(struct racional r1, struct racional r2){
+  if ((!validor_r(r1)||(!valido_r(r2))))
+    return();//inválido
+
+  struct racional rSubt;
+  rSubt.den= mmc(r1.den, r2.den); //denominador do r resultado da subtração, necessita do mmc dos denominadores dos racionais a serem subtraídos
+  rSubt.num= ((r1.num*(rSubt.den/r1.den))-(r2.num*(rSubt.den/r2.den))); //o numerador do r resultado, é a subtração dos numeradores dos racionas,com cada um multiplicado do mmc dos denominadores
+  return(rSubt);
+}
+
+struct racional multiplica_r (struct racional r1, struct racional r2){
+  if ((!validor_r(r1)||(!valido_r(r2))))
+    return();//inválido
+  
+  struct racional rMult;
+  rMult.num= (r1.num)*(r2.num); //numerador do r resultado, é o produto dos numeradores dos racionais a serem multiplicados
+  rMult.den= (r1.den)*(r2.den); //denominador do r resultado, é o produto dos denominadores dos racionais a serem multiplicados
+  return(rMult);
+}
+
+struct racional divide_r (struct racional r1, struct racional r2){
+  if ((!validor_r(r1)||(!valido_r(r2))))
+    return();//inválido
+  
+  struct racional rDiv;
+  rDiv.num= (r1.num)*(r2.den); //numerador do r resultado, é o produto do numerador do 1ºracional com o denominador do 2º racional a ser multiplicado
+  rDiv.den= (r1.den)*(r2.num); //denominador do r resultado, é o produto do denominador do 1ºracional com o numerador do 2º racional a ser multiplicado
+  return(rDiv);
+}
