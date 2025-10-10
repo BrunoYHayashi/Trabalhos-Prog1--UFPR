3e42afe386005ead67eb2dce7fd408d40457b8a7
BrunoYHayashi <byh25@inf.ufpr.br>
2025-10-10 02:55:21 -0300

    [MOD] fiz as outras funcoes de lista.c , testar e corrigir erros

--- Trabalho4/tp4/lista.c
+++ Trabalho4/tp4/lista.c
@@ -85,29 +85,99 @@ int lista_insere_ordenado (struct lista *lista, int chave){
 int lista_remove_inicio (struct lista *lista, int *chave){
     struct nodo *nodoRemovido;
 
+    if (!lista)
+        return 0;
+
     nodoRemovido = lista->ini;
     lista->ini = lista->ini->prox;
+    lista->tamanho = lista->tamanho -1;
 
     free(nodoRemovido);
+
+    return (1);
 }
 
 int lista_remove_fim (struct lista *lista, int *chave){
+    struct nodo *nodoRemovido;
+
+    if (!lista)
+        return 0;
+
+    lista->ptr = lista->ini;
+
+    while (lista->ptr->prox != NULL)
+        lista->ptr = lista->ptr->prox;
+
+    nodoRemovido = lista->ptr;
+    lista->ptr = NULL;
+    lista->tamanho = lista->tamanho -1;
+
+    free(nodoRemovido);
+
+    return 1;
+
 }
 
 int lista_remove_ordenado (struct lista *lista, int chave){
+    struct nodo *nodoRemovido
+
+    if (!lista)
+        return 0;
+
+    lista->ptr = lista->ini;
+
+    while (lista->ptr->chave != chave)
+        lista->ptr = lista->ptr->prox;
+
+    nodoRemovido = lista->ptr;
+    lista->ptr = NULL;
+    lista->tamanho = lista->tamanho -1;
+
+    free (nodoRemovido);
+
+    return 1;
+
 }
 
 int lista_vazia (struct lista *lista){
+    if (lista->ini = NULL)
+        return 1;
+
+    return 0;
 }
 
 int lista_tamanho (struct lista *lista){
+    return (lista->tamanho);
 }
 
 int lista_pertence (struct lista *lista, int chave){
+    lista->ptr = lista->ini;
+    while ((lista->ptr != NULL) ) {
+        if (lista->ptr->chave = chave)
+            return 1;
+
+        lista->ptr = lista->ptr->prox;
+    }
+
+    return 0;
 }
 
 void lista_inicia_iterador (struct lista *lista){
+
+    lista->ptr = lista->ini;
+    
+    while (lista->ptr->prox != NULL)
+        lista_incrementa_iterador(lista, lista->ptr->chave);
+-    
 }
 
 int lista_incrementa_iterador (struct lista *lista, int *chave){
+    
+    if (lista->ptr->prox == NULL)
+        return 0;
+
+    lista->ptr = lista->ptr->prox; 
+    chave = lista->ptr->chave;
+
+    return 1;
 }
