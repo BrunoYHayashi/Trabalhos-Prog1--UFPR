6f477bded80b7d94bcd82f60ca4b6bfb12cb06a9
BrunoYHayashi <byh25@inf.ufpr.br>
2025-10-26 18:30:35 -0300

    [ADD] funções de fprio.h e finalizada função que cria e destrói a fila

--- Trabalho5/tp5/fprio.c
+++ Trabalho5/tp5/fprio.c
@@ -21,3 +21,63 @@ struct fprio_t
 } ;
 
 // A COMPLETAR
+
+struct fprio_t *fprio_cria (){
+  struct fprio_t *fprioCriada;
+  if (!(fprioCriada=malloc (sizeof(struct fprio_t)))) //tenta alocar de forma dinâmica uma memória para a fila. 
+    return NULL; //Caso falhe, retorna NULL
+
+  fprioCriada->prim = NULL; //Primeiro nodo da fila é NULL
+  fprioCriada->num = 0; //Número de ítens da fila é 0
+  
+  return fprioCriada;
+}
+
+struct fprio_t *fprio_destroi (struct fprio_t *f){
+
+  if (!f) //verifica se f é válido
+    return NULL; //se não for, retorna NULL
+
+  struct fpnodo_t *atual, *prox;
+
+  atual = f->prim;
+  
+  while (atual != NULL){ //da free para todos os nodos da fila até que atual (nodo atual) seja NULL, nesse caso, não existe mais nodos na fila
+    prox = atual->prox;
+    free (atual->item); //da free no item do nodo
+    free (atual); //da free no atual
+    atual = prox;
+  }
+
+  free(f); //da free na fila
+  return NULL;
+}
+
+// Insere o item na fila, mantendo-a ordenada por prioridades crescentes.
+// Itens com a mesma prioridade devem respeitar a politica FIFO (retirar
+// na ordem em que inseriu).
+// Inserir duas vezes o mesmo item (o mesmo ponteiro) é um erro.
+// Retorno: número de itens na fila após a operação ou -1 se erro.
+int fprio_insere (struct fprio_t *f, void *item, int tipo, int prio){
+
+}
+
+// Retira o primeiro item da fila e o devolve; o tipo e a prioridade
+// do item são devolvidos nos parâmetros "tipo" e "prio".
+// Retorno: ponteiro para o item retirado ou NULL se fila vazia ou erro.
+void *fprio_retira (struct fprio_t *f, int *tipo, int *prio){
+
+}
+
+// Informa o número de itens na fila.
+// Retorno: N >= 0 ou -1 se erro.
+int fprio_tamanho (struct fprio_t *f){
+
+}
+
+// Imprime o conteúdo da fila no formato "(tipo prio) (tipo prio) ..."
+// Para cada item deve ser impresso seu tipo e sua prioridade, com um
+// espaço entre valores, sem espaços antes ou depois e sem nova linha.
+void fprio_imprime (struct fprio_t *f){
+
+}
