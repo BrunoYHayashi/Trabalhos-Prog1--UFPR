62c8ce5be96a42635ba48dcf02a74d219db426e2
BrunoYHayashi <byh25@inf.ufpr.br>
2025-11-23 18:45:46 -0300

    [MOD] adição das constantes e definição das funções

--- TheBoys/theboys/events.c
+++ TheBoys/theboys/events.c
@@ -3,3 +3,21 @@
 
 #include "events.h"
 #include "fprio.h"
+#include "fila.h"
+#include "entities.h"
+#include "conjunto.h"
+#include "environment.h"
+
+void schedule_event (struct world *w, int time, int tipo, struct hero *hero, struct base *base, struct mission *mission){
+    struct event *event;
+
+    if(!(event=malloc(sizeof(struct event)))) //Aloca memória para o evento
+        return;
+
+    /*Atribui ao evento o herói afetado, a base afetada, ou a missão*/
+    event->hero= hero;
+    event->base=base;
+    event->mission=mission;
+
+    fprio_insere(w->lef, event, tipo, time); //Insere na fila de prioridades o evento
+}
--- TheBoys/theboys/events.h
+++ TheBoys/theboys/events.h
@@ -1,6 +1,40 @@
 #ifndef EVENTS
 #define EVENTS
 
+#include "entities.h"
 
+/*definição dos (int tipo) dos eventos*/
+#define EVENT_CHEGA 1
+#define EVENT_ESPERA 2
+#define EVENT_DESISTE 3
+#define EVENT_AVISA 4
+#define EVENT_ENTRA 5
+#define EVENT_SAI 6
+#define EVENT_VIAJA 7
+#define EVENT_MORRE 8
+#define EVENT_MISSAO 9
+#define EVENT_FIM 10
+
+/*Estrutura dos eventos, nem todo evento conterá as 3 structs de composição*/
+struct event {
+    struct hero *hero;
+    struct base *base;
+    struct mission *mission;
+};
+
+/*Funções dos eventos, todos recebem o ponteiro para o mundo, o tempo e uma struct event*/
+void event_chega (struct world *w, int time, struct event *data);
+void event_espera (struct world*w, int time, struct event *data);
+void event_desiste (struct world*w, int time, struct event *data);
+void event_avisa (struct world*w, int time, struct event *data);
+void event_entra (struct world*w, int time, struct event *data);
+void event_sai (struct world*w, int time, struct event *data);
+void event_viaja (struct world*w, int time, struct event *data);
+void event_morre (struct world*w, int time, struct event *data);
+void event_missao (struct world*w, int time, struct event *data);
+void event_fim (struct world*w, int time, struct event *data);
+
+/*Funções auxiliares*/
+void schedule_event (struct world *w, int time, int tipo, struct hero *hero, struct base *base, struct mission *mission);
 
 #endif
